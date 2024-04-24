e1212e58f69174d737257a3dc85f0ded8cbbea9b
santicrak2017 <szamorag@unal.edu.co>
2024-04-23 23:24:01 -0500

    Este es el cambio para la version experimental

--- src/main/java/com/mycompany/paraclase/Paraclase.java
+++ src/main/java/com/mycompany/paraclase/Paraclase.java
@@ -9,10 +9,34 @@ package com.mycompany.paraclase;
  * @author User
  */
 public class Paraclase {
+String Nombre;
+int numero;
+
+    public Paraclase(String Nombre, int numero) {
+        this.Nombre = Nombre;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
         System.out.println("HOGHOFHD");
+        
     }
 }
